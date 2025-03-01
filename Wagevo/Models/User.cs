49e720b72799bbbc725cbe10d28132257c6eb4f9
BenjiJackson1 <blj55@cornell.edu>
2025-03-01 16:00:52 -0700

    Updating User DB model

--- Wagevo/Models/User.cs
+++ Wagevo/Models/User.cs
@@ -2,7 +2,15 @@ namespace Wagevo.Models;
 
 public class User
 {
-    public int Id { get; set; }  // Primary Key
-    public string Name { get; set; }
+    public int UserId { get; set; }  // Primary Key
+    public int  CompanyId { get; set; }
+    public bool IsAdmin { get; set; }
+    public DateTime DateCreated { get; set; }
     public string Email { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime Birthday { get; set; }
+    public double HourlyWage { get; set; }
+    public required string Password { get; set; }
 }
